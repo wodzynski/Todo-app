aef26fc06311976d491012a962cf25989f9a82d8
wodzynski <macko5395@gmail.com>
2018-06-10 16:35:18 +0200

    Added a very nice HTML Tag remover :)

--- Assets/js/todos.js
+++ Assets/js/todos.js
@@ -1,1 +1,26 @@
-alert("connected")
+// Check Off Specific Todos By Clicking
+$('ul').on('click', 'li', function() {
+    $(this).toggleClass('completed');
+});
+
+// Click on X to delete todo
+$('ul').on('click', 'span', function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+// Add a listener to the input
+$('input[type="text"]').keypress(function(event) {
+    if (event.which === 13) {
+        // grabbing a new todo text from input
+        var regex = /(<([^>]+)>)/ig,
+            body = $('input').val(),
+            result = body.replace(regex, "");
+        // empty the input
+        $(this).val('');
+        // create a new li and add to ul
+        $('ul').append('<li><span>X </span>' + result + '</li>');
+    }
+});
